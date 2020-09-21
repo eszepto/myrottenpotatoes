c18a132485f1f67fba08f1e7184acbab866dbca9
Pattaranan Srisara <pattaranan2000@gmail.com>
2020-09-21 22:58:28 +0700

    Add twitter third party authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
+  
+  protect_from_forgery with: :exception
+  before_action :set_current_user, :authenticate!
+  
+  def set_current_user
+      @current_user = Moviegoer.find_by(id: session[:user_id])
+  end
+  
+  protected
+  
+  def authenticate!
+      unless @current_user
+          redirect_to login_path
+      end
+  end
 end
-    
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  resources :movies
-  root :to => redirect('/movies')
-
   get  'auth/:provider/callback' => 'sessions#create'
   post 'logout' => 'sessions#destroy'
   get  'auth/failure' => 'sessions#failure'
   get  'auth/twitter', :as => 'login'
+  
+  resources :movies
+  root :to => redirect('/movies')
+
+  
 end
