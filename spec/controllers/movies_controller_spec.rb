33438f7729153d1b7fcb0792406d5aff0718d725
Pattaranan Srisara <pattaranan2000@gmail.com>
2020-10-18 02:01:00 +0700

    Add 'Add movie' Unit test using rspec

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'date'
 
 describe MoviesController do
   describe 'searching TMDb' do
@@ -23,4 +24,53 @@ describe MoviesController do
       end
     end
   end
+
+  describe 'Add a movie' do
+    before :each do
+      @fake_result = [double('movie1')]
+      get :new
+    end
+    it 'selects the new template for rendering' do
+      expect(response).to render_template('new')
+      post :create, params: {
+        movie: {
+          :title => 'Aladdin',
+          :rating => 'G',
+          :release_date => '25-Nov-1992', 
+          :description => 'Aladdin blah blah'
+        }
+      }
+    end
+
+    describe 'after add new movie' do
+      before :each do
+        post :create, params: {
+          movie: {
+            :title => 'Aladdin',
+            :rating => 'G',
+            :release_date => '25-Nov-1992', 
+            :description => 'Aladdin blah blah'
+          }
+        }
+      end
+      it 'redirect to show template for rendering' do
+        expect(response).to redirect_to(:controller => 'movies', :action => 'show', :id => 1)
+      end
+      it 'makes the new movie result available on that template' do
+        expect(assigns(:movie)).to have_attributes(
+          :title => 'Aladdin',
+          :rating => 'G', 
+          :release_date => DateTime.parse('25-Nov-1992'), 
+          :description => 'Aladdin blah blah')
+      end
+    end
+
+  end
+  describe 'Edit movie' do
+    
+  end
+  describe 'Delete movie' do
+    
+  end
 end
+
