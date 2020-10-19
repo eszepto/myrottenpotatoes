f84df28443479833d62607c699759a19e3d5c819
Pichet <bestoinw@hotmail.com>
2020-10-20 00:13:12 +0700

    use dotenv for twitter key

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 # Replace API_KEY and API_SECRET with the values you got from Twitter
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :twitter, "GbDCIN405rPcTxD88yriSYFLc", "yxM4EfFXulXAyE24urA5sNYJ2wmVcIUOIm1EqyIwjNvdaHAEQA"
+    provider :twitter, ENV["TWITTER_KEY"], ENV["TWITTER_SECRET"]
   end
--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,4 @@ Rails.application.routes.draw do
   end
   root :to => redirect('/movies')
 
-  
-  
-
 end
